fa582659f8ae3d426a90740d331027fd30f8a1b9
Andrew Ray <aray40@gatech.edu>
2017-07-14 23:39:11 -0400

    Supply Credentials through Build Arguments in Drupal
    Passwords were passed in through .tar.gz files, which was unacceptable for security. Instead, the password is changed dynamically upon setup.

--- docker/custom_images/travel_db/_start.sh
+++ docker/custom_images/travel_db/_start.sh
@@ -2,6 +2,10 @@
 touch /locks/db.lock
 if [ ! -e /locks/initialized.status ]; then
     psql -f /schemas/drupal.sql postgres
+    pass="PASSWORD '$POSTGRES_PASSWORD'"
+    psql -U postgres -v ON_ERROR_STOP=1 -c \
+        "ALTER USER $POSTGRES_USER WITH NOSUPERUSER $pass;"
+    unset pass
     psql -f /schemas/portaluser.sql drupal
     psql -U portaluser -f /schemas/portal.sql drupal
     psql -U postgres -v ON_ERROR_STOP=1 -c \
